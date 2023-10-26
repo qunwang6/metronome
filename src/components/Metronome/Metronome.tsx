24bce02051c4f2af35c91b97b0652a57d439ea1e
qunwang6 <qunwang6@gmail.com>
2023-10-26 14:10:18 +0800

    fff

--- src/components/Metronome/Metronome.tsx
+++ src/components/Metronome/Metronome.tsx
@@ -151,12 +151,7 @@ const Metronome = () => {
       Tone.Transport.start()
       // Tone.context.resume()
       // alert("start211")
-      const synth = new Tone.Synth().toDestination();
-const now = Tone.now()
-// trigger the attack immediately
-synth.triggerAttack("C4", now)
-// wait one second before triggering the release
-synth.triggerRelease(now + 1)
+
 
 
 
